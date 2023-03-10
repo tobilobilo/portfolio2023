8b1b7839f71d11b6c9f694032447da927c55daf0
JP Rose <test@test.com>
2023-03-10 16:22:57 -0500

    ajout template html, fix accessibilité, ajout data

---
+++ js/data.js
@@ -0,0 +1,174 @@
+const skills = {
+    'HTML': {
+        'icon': 'svg/skill-html.svg',
+        'subskills': ['Sémantique', 'Accessibilité', 'Twig'],
+        'animationdelay': '.4s',
+        'extraclass': 'skills_logo--smaller'
+    },
+    'CSS': {
+        'icon': 'svg/skill-css.svg',
+        'subskills': ['SASS / LESS', 'Bootstrap', 'BEM'],
+        'animationdelay': '.55s',
+        'extraclass': 'skills_logo--smaller'
+    },
+    'Javascript': {
+        'icon': 'svg/skill-js.svg',
+        'subskills': ['ES6+', 'Vue.js', 'jQuery'],
+        'animationdelay': '.7s',
+        'extraclass': null
+    },
+    'UI / UX': {
+        'icon': 'svg/skill-window.svg',
+        'subskills': ['Photoshop', 'Illustrator', 'Figma'],
+        'animationdelay': '.85s',
+        'extraclass': null
+    },
+    'Outils': {
+        'icon': 'svg/skill-toolbox.svg',
+        'subskills': ['Git', 'Drupal', 'Wordpress'],
+        'animationdelay': '1s',
+        'extraclass': null
+    },
+}
+
+
+const projects = {
+    'Studios MELS': {
+        'type': 'pro',
+        'text': [
+            'Site web corporatif des Studios MELS développé sous Wordpress.',
+            'Reconnue mondialement pour ses studios de tournage et sa main d\'œuvre hautement qualifiée, MELS est l\'une des plus grandes entreprises de services de cinéma et de télévision au Canada. Elle propose un service clé en main, du scénario à la livraison.'
+        ],
+        'links': {
+            'demo': 'https://www.mels-studios.com/fr/',
+            'github': null
+        },
+        'imgs': {
+            'preview': {
+                'small': './images/previews/mels/mels_bg.jpg',
+                'large': './images/previews/mels/mels_bg__lg.jpg'
+            },
+            'desktop': {
+                'small': './images/previews/mels/mels_desktop.jpg',
+                'large': './images/previews/mels/mels_desktop__lg.jpg'
+            },
+            'mobile': './images/previews/mels/mels_mobile.jpg'
+        },
+        'stack': ['Wordpress']
+    },
+    'Groupe TVA': {
+        'type': 'pro',
+        'text': [
+            'Site web corporatif du Groupe TVA développé sous Drupal.',
+            'Groupe TVA est une entreprise québécoise active dans les domaines de la télévision, du film et des magazines. Elle est une propriété de Québecor Média. Les principaux actifs de l\'entreprise incluent le réseau TVA ainsi que six de ses dix stations régionales et des chaînes spécialisées.'
+        ],
+        'links': {
+            'demo': 'https://www.groupetva.ca/',
+            'github': null
+        },
+        'imgs': {
+            'preview': {
+                'small': './images/previews/groupetva/groupetva_bg.jpg',
+                'large': './images/previews/groupetva/groupetva_bg__lg.jpg'
+            },
+            'desktop': {
+                'small': './images/previews/groupetva/groupetva_desktop.jpg',
+                'large': './images/previews/groupetva/groupetva_desktop__lg.jpg'
+            },
+            'mobile': './images/previews/groupetva/groupetva_mobile.jpg'
+        },
+        'stack': ['Drupal']
+    },
+    'Mastermind': {
+        'type': 'perso',
+        'text': [
+            'Application du jeu Mastermind comprenant des fonctionnalités supplémentaires, comme la possibilité de choisir le nombre de couleurs, de colonnes et de rangées de combinaisons pour trouver la solution.',
+            'Ce projet aurait été idéal à développer à l\'aide d\'un framework, mais je m\'étais donné comme défi de le faire en pur Javascript.'
+        ],
+        'links': {
+            'demo': 'https://tobilobilo.github.io/mastermind/',
+            'github': 'https://github.com/tobilobilo/mastermind'
+        },
+        'imgs': {
+            'preview': {
+                'small': './images/previews/mastermind/mastermind_bg.png',
+                'large': './images/previews/mastermind/mastermind_bg__lg.png'
+            },
+            'desktop': {
+                'small': './images/previews/mastermind/mastermind_desktop.png',
+                'large': './images/previews/mastermind/mastermind_desktop__lg.png'
+            },
+            'mobile': './images/previews/mastermind/mastermind_mobile.png'
+        },
+        'stack': ['ES6']
+    },
+    'Bingo': {
+        'type': 'perso',
+        'text': [
+            'Application du jeu de Bingo avec quelques aspects paramétrables tels que : le nombre de joueurs et le type de grilles. Deux choix de grilles sont disponibles (le classique 5x5 et une version 9x9).',
+            'Ce projet a été développé avec la version CLI de Vue.js 2.'
+        ],
+        'links': {
+            'demo': 'https://tobilobilo.github.io/bingo/',
+            'github': 'https://github.com/tobilobilo/bingo'
+        },
+        'imgs': {
+            'preview': {
+                'small': './images/previews/bingo/bingo_bg.png',
+                'large': './images/previews/bingo/bingo_bg__lg.png'
+            },
+            'desktop': {
+                'small': './images/previews/bingo/bingo_desktop.png',
+                'large': './images/previews/bingo/bingo_desktop__lg.png'
+            },
+            'mobile': './images/previews/bingo/bingo_mobile.png'
+        },
+        'stack': ['Vue.js', 'Bootstrap']
+    },
+    'Agrégateur d\'annonces Kijiji': {
+        'type': 'perso',
+        'text': [
+            'Application qui récupère différents flux RSS provenant de Kijiji et les affiches automatiques aux 30 secondes.',
+            'C\'est un projet que j\'ai développé, il y a plusieurs années et dont je me sers encore fréquemment pour trouver des objets de seconde main ou de collection.'
+        ],
+        'links': {
+            'demo': 'http://mads.epizy.com/?p=demo',
+            'github': 'https://github.com/tobilobilo/kijiji-scrapper'
+        },
+        'imgs': {
+            'preview': {
+                'small': './images/previews/scrapper/scrapper_bg.png',
+                'large': './images/previews/scrapper/scrapper_bg__lg.png'
+            },
+            'desktop': {
+                'small': './images/previews/scrapper/scrapper_desktop.jpg',
+                'large': './images/previews/scrapper/scrapper_desktop__lg.jpg'
+            },
+            'mobile': './images/previews/scrapper/scrapper_mobile.jpg'
+        },
+        'stack': ['jQuery', 'PHP']
+    },
+    'Tic Tac Toe': {
+        'type': 'perso',
+        'text': [
+            'Application du jeu classique Tic Tac Toe comprenant des fonctionnalités supplémentaires, telles que : la possibilité de jouer jusqu\'à 4 joueurs, de nommer les joueurs et d\'assurer le suivi des statistiques. La grille de jeu est modifiable, allant du classique 3x3 jusqu\'à 10x10.',
+            'Ce projet a été développé avec la version précompilée de Vue.js 2. Il a été mon projet initial sous ce framework.'
+        ],
+        'links': {
+            'demo': 'https://tobilobilo.github.io/tic-tac-toe/',
+            'github': 'https://github.com/tobilobilo/tic-tac-toe'
+        },
+        'imgs': {
+            'preview': {
+                'small': './images/previews/tictactoe/tictactoe_bg.png',
+                'large': './images/previews/tictactoe/tictactoe_bg__lg.png'
+            },
+            'desktop': {
+                'small': './images/previews/tictactoe/tictactoe_desktop.png',
+                'large': './images/previews/tictactoe/tictactoe_desktop__lg.png'
+            },
+            'mobile': './images/previews/tictactoe/tictactoe_mobile.png'
+        },
+        'stack': ['Vue.js', 'Bootstrap']
+    }
+}
--- js/scripts.js
+++ js/scripts.js
@@ -22,24 +22,26 @@ if(!showInto) { // 'show complete intro animation'
 /*
     Parallax triggers binding
 */
-const controller = new ScrollMagic.Controller();
-document.querySelectorAll('[data-fade-trigger]').forEach((el) => {
-    const id = el.getAttribute('id');
-    // build scene - fade in stuff
-    new ScrollMagic.Scene({
-        triggerElement: `#${id}`,
-        triggerHook: .9,
-        offset: 150, // move trigger to center of element
-        reverse: false // only do once
-    })
-    //.addIndicators() // add indicators (requires plugin)
-    .on("enter leave", function (e) {
-        setTimeout(function() {
-            toFadeIn(document.querySelectorAll(`#${id} [data-tofadein]`));
-        }, 250);
-    })
-    .addTo(controller);
-});
+const bindParallaxTriggers = () => {
+    const controller = new ScrollMagic.Controller();
+    document.querySelectorAll('[data-fade-trigger]').forEach((el) => {
+        const id = el.getAttribute('id');
+        // build scene - fade in stuff
+        new ScrollMagic.Scene({
+            triggerElement: `#${id}`,
+            triggerHook: .9,
+            offset: 150, // move trigger to center of element
+            reverse: false // only do once
+        })
+        //.addIndicators() // add indicators (requires plugin)
+        .on("enter leave", function (e) {
+            setTimeout(function() {
+                toFadeIn(document.querySelectorAll(`#${id} [data-tofadein]`));
+            }, 250);
+        })
+        .addTo(controller);
+    });
+}
 
 
 /*
@@ -96,3 +98,102 @@ document.querySelector('#switch-theme').addEventListener('change', (e) => {
     document.documentElement.setAttribute('data-theme', (e.target.checked) ? 'dark' : 'light');
     //document.querySelector('#switch-label-1').textContent = document.querySelector('#switch-label-1').getAttribute((e.target.checked) ? 'data-text-off' : 'data-text-on');
 });
+
+// Mail to
+document.querySelectorAll('[data-event="mailto"]').forEach( item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        const   zero   = 'mailt',
+                one    = 'o:jipr',
+                two    = 'ose@gm',
+                three  = 'ail.c',
+                four   = 'om';
+        location = `${zero}${one}${two}${three}${four}`;
+    });
+});
+
+/*
+    Append content from templates
+*/
+function appendTemplateElements() {
+    //Skills
+    const wrapperSkills = document.querySelector('[data-element="skills"]');
+    const fragmentSkills = new DocumentFragment();
+    for(let skill in skills) {
+        const templateSkill = document.querySelector('[data-template="skill"]');
+        const content = templateSkill.content.cloneNode(true);
+        content.querySelector('.skills__subtitle').innerText = skill;
+        content.querySelector('.skills__logo').setAttribute('alt', skill);
+        content.querySelector('.skills__logo').setAttribute('src', skills[skill].icon);
+        content.querySelector('.skills__logo').classList.add(skills[skill].extraclass);
+        content.querySelector('.skills__table').style.animationDelay = skills[skill].animationdelay;
+        const skillsList = content.querySelector('.skills__list');
+        const templateSkillItem = content.querySelector('[data-template="skill-item"]');
+        for(let subskill in skills[skill].subskills) {
+            const item = templateSkillItem.content.cloneNode(true);
+            item.querySelector('.skills__item').innerText = skills[skill].subskills[subskill];
+            skillsList.append(item);
+        }
+        fragmentSkills.append(content);
+    }
+    wrapperSkills.append(fragmentSkills);
+
+    //Projects
+    const wrapperProjectPro = document.querySelector('[data-element="project-pro"]');
+    const wrapperProjectPerso = document.querySelector('[data-element="project-perso"]');
+    const fragmentProjectPro = new DocumentFragment();
+    const fragmentProjectPerso = new DocumentFragment();
+    for(let project in projects) {
+        const templateProject = document.querySelector('[data-template="project"]');
+        const content = templateProject.content.cloneNode(true);
+        content.querySelector('.project__title').innerText = project;
+        content.querySelector('.project').id = `fade-trigger-${projects[project].type}-${Object.keys(projects).indexOf(project)}`;
+        const btnDemo = content.querySelector('.project__btn--demo');
+        (projects[project].links.demo) ? btnDemo.setAttribute('href', projects[project].links.demo) : btnDemo.remove();
+        const btnGithub = content.querySelector('.project__btn--github');
+        (projects[project].links.github) ? btnGithub.setAttribute('href', projects[project].links.github) : btnGithub.remove();
+        const previewMobile = content.querySelector('.project-imgs__preview.mobile .project-imgs__img');
+        previewMobile.setAttribute('src', projects[project].imgs.mobile);
+        previewMobile.setAttribute('alt', previewMobile.getAttribute('alt').replace('%%', project));
+        const imgDesktop = content.querySelector('.project-imgs__preview.desktop .project-imgs__img');
+        const imgDesktopLarge = content.querySelector('.project-imgs__preview.desktop source');
+        imgDesktop.setAttribute('src', projects[project].imgs.desktop.small);
+        imgDesktopLarge.setAttribute('srcset', projects[project].imgs.desktop.large);
+        imgDesktop.setAttribute('alt', imgDesktop.getAttribute('alt').replace('%%', project));
+        const previewDesktop = content.querySelector('.project-preview__picture .project-preview__img');
+        const previewDesktopLarge = content.querySelector('.project-preview__picture source');
+        previewDesktop.setAttribute('src', projects[project].imgs.preview.small);
+        previewDesktopLarge.setAttribute('srcset', projects[project].imgs.preview.large);
+        previewDesktop.setAttribute('alt', previewDesktop.getAttribute('alt').replace('%%', project));
+        //content.querySelector('.skills__logo').setAttribute('src', projects[project].icon);
+        //content.querySelector('.skills__logo').classList.add(projects[project].extraclass);
+        //content.querySelector('.skills__table').style.animationDelay = projects[project].animationdelay;
+        const projectsStacks = content.querySelector('.project__stack');
+        const templateStack = content.querySelector('[data-template="project-stack"]');
+        for(let tech in projects[project].stack) {
+            const item = templateStack.content.cloneNode(true);
+            item.querySelector('li').innerText = projects[project].stack[tech];
+            projectsStacks.append(item);
+        }
+        const projectsPars = content.querySelector('.project__summary');
+        const templatePar = content.querySelector('[data-template="project-par"]');
+        for(let par in projects[project].text) {
+            const item = templatePar.content.cloneNode(true);
+            item.querySelector('p').innerText = projects[project].text[par];
+            projectsPars.append(item);
+        }
+        switch (projects[project].type) {
+            case "pro":
+                fragmentProjectPro.append(content);
+                break;
+            default:
+                fragmentProjectPerso.append(content);
+                break;
+        }
+        
+    }
+    wrapperProjectPro.append(fragmentProjectPro);
+    wrapperProjectPerso.append(fragmentProjectPerso);
+
+    bindParallaxTriggers();
+}
